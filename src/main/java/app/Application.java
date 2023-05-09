2d56839838ec9be0c3fe31a7ce5b56eff3f154e5
tumanova <aleksandrasemenova92186@gmail.com>
2023-05-09 16:40:32 +0300

    java doc

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,14 +3,28 @@ package app;
 import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
+/**
+ * Класс окна приложения
+ */
 
 public class Application implements Consumer<Event> {
+    /**
+     *
+     */
         private final Window window;
+    /**
+     * Конструктор окна приложения
+     */
         public Application() {
             window = App.makeWindow();
             window.setEventListener(this);
             window.setVisible(true);
         }
+
+    /**
+     * Обработчик событий
+     * @param event событие
+     */
     @Override
     public void accept(Event event) {
         if (event instanceof EventWindowClose) {
